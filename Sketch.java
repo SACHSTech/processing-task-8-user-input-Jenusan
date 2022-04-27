472cf0d30350b27f1fc0cb5780593a72c1411887
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-04-27 18:43:08 -0400

    DONe

--- Sketch.java
+++ Sketch.java
@@ -1,14 +1,17 @@
+import org.w3c.dom.Text;
+
 import processing.core.PApplet;
+import processing.core.PImage;
+
 
 public class Sketch extends PApplet {
-	
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(800, 800);
   }
 
   /** 
@@ -16,21 +19,85 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(31, 153, 255);
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  public void draw() { 
+    }
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
-  
-  // define other methods down here.
+    public void mouseClicked() {
+      fill(153, 153, 153);
+      rect(mouseX, mouseY, 150, 800);
+
+      for (int i=0; i< (800-mouseY); i++){
+        fill(53);
+        rect(mouseX + 15, mouseY + 20 + (i*60), 45,45);
+        rect(mouseX + 85, mouseY + 20 + (i*60), 45,45);
+      }
+    }
+
+
+    //public void mouseMoved() {
+    //fill(0);
+    //ellipse(mouseX,mouseY,3,3);
+    //}
+
+    public void mouseDragged(){
+      PImage birds = loadImage("birds.png");
+      birds.resize(100,100);
+      image(birds, mouseX, mouseY);
+    }
+
+    public void mousePressed(){
+      if (mousePressed){
+      PImage cloud = loadImage("cloud.png");
+      cloud.resize(50,50);
+      image(cloud, random(0,800), random(0,200));
+      }
+    }
+    public void mouseReleased(){
+      PImage cloud2 = loadImage("cloud 2.png");
+      cloud2.resize(50,50);
+      image(cloud2, random(0,800), random(0,200));
+    }
+
+    public void mouseWheel(){
+    fill(153,102,0);
+    rect(mouseX - 10, mouseY - 10, 20, 800);
+
+    fill(0,153,0); 
+    ellipse(mouseX + 25, mouseY,50,50);
+
+    fill(0,153,0); 
+    ellipse(mouseX - 25, mouseY,50,50);
+
+    fill(0,153,0); 
+    ellipse(mouseX , mouseY - 25,50,50);
+    }
+
+
+    public void keyReleased(){
+      textSize(100);
+      fill(0);
+      text("KANYEVILLE", 100, 100);
+    }
+
+    public void keyPressed(){
+      PImage ye = loadImage("ye.png");
+      ye.resize(100,100);
+      PImage kanye = loadImage("kanye.png");
+      kanye.resize(200,200);
+
+      if (keyCode == ENTER){
+        image(kanye, random(0,800), random(0,800));
+      }
+
+      if (key == 'y'){
+        image(ye, random(0,800), 700);
+      }
+    }
 }
